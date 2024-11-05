86231c31fecaf6da21ee7aed55ead7410805be3a
0ri6 <oriol.font.7e8@itb.cat>
2024-11-05 09:38:06 +0100

    Actualización HTML

---
+++ act_javascript/Ex1/file.js
@@ -0,0 +1,7 @@
+document.querySelector("button").addEventListener("click", function () {
+
+
+    const nom =
+        document.getElementById("nameInput").value;
+        document.getElementById("result"). innerText = "Hola " + nom + " Benvingut!!";
+});
---
+++ act_javascript/Ex2/file.js
@@ -0,0 +1,12 @@
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        const resultParagraphs = document.querySelectorAll(".result");
+
+        resultParagraphs.forEach(paragraph => {
+            paragraph.innerText = "One button clicked";
+        });
+    });
+})
