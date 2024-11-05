53de9e12ef035679ee5baaf31a5235a36027739f
0ri6 <oriol.font.7e8@itb.cat>
2024-11-05 11:20:27 +0100

    Actualización HTML

--- act_javascript/Ex3/file.js
+++ act_javascript/Ex3/file.js
@@ -6,5 +6,5 @@
         p.style.backgroundColor = "#90EE90";
         p.style.padding = "10px";
         p.style.fontWeight = "bold";
-        p.style.border = "2px solid Black"
+        p.style.border = "2px solid Black";
     }
---
+++ act_javascript/Ex4/file.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("button");
+const p = document.querySelector("p");
+button.addEventListener("click", colorear);
+
+function colorear() {
+    p.classList.togg("colorleear")
+}
+
+
