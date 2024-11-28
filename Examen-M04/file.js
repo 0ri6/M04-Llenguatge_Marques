1a7af8a759635979564a82cc21d45c7587900f62
0ri6 <oriol.font.7e8@itb.cat>
2024-11-28 10:45:16 +0100

    Actualización HTML

--- Examen-M04/file.js
+++ Examen-M04/file.js
@@ -1,6 +1,23 @@
+//Mostri 👏 per cada click
+
 document.addEventListener('DOMContentLoaded', function (){
     document.querySelector('.text').addEventListener('click', function (){
         this.innerHTML += '👏';
     });
 });
 
+//
+document.addEventListener('DOMContentLoaded', function() {
+    const zoom = document.querySelector('.grid');
+    const paragraph = document.getElementById('.papallona');
+
+    zoom.addEventListener('mouseenter', function() {
+        paragraph.style.fontSize = '24px';
+
+
+    });
+
+    zoom.addEventListener('mouseleave', function() {
+        paragraph.style.fontSize = '16px';
+    });
+});
