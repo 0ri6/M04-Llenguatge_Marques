793d54f31a3e5eefce9cff44e57baefc942ce312
0ri6 <oriol.font.7e8@itb.cat>
2024-11-28 10:35:16 +0100

    Actualización HTML

---
+++ Examen-M04/file.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function (){
+    document.querySelector('.text').addEventListener('click', function (){
+        this.innerHTML += '👏';
+    });
+});
+
